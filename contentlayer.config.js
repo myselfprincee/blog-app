743b2fc968271cfaa8cef6cc56161c2b21207da6
Prince Gupta <prince.kumar01129@gmail.com>
2024-01-15 13:52:13 +0530

    feat : Configured Contentlayer

---
+++ contentlayer.config.js
@@ -0,0 +1,20 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.csv`,
+  contentType: 'pdf',
+  fields: {
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (doc) => `/posts/${doc._raw.flattenedPath}`,
+    },
+  },
+}))
+
+export default makeSource({
+  contentDirPath: '',
+  documentTypes: [Post],
+})
